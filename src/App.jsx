22ca9945d2ede1ee1d6e33f0b78bbe6d26361eb0
aminaibrahimm <ai7687772@gmail.com>
2025-10-25 23:22:47 +0200

    add sidebar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+import Sidebar from './components/sidebar/Sidebar'
+// import { createBrowserRouter } from 'react-router-dom'
+function App() {
+  // createBrowserRouter()
+  return (
+    <>
+      <Navbar/>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,274 @@
+import React, { useState } from 'react'
+import { SlHome } from "react-icons/sl";
+import { HiOutlineSquares2X2 } from "react-icons/hi2";
+import { FaAngleUp, FaChevronDown, FaUsers } from "react-icons/fa6";
+import { MdOutlineCardTravel, MdOutlineWbIncandescent } from "react-icons/md";
+import { PiCertificateLight } from "react-icons/pi";
+import { GrCatalogOption } from "react-icons/gr";
+import { LuFilePenLine } from "react-icons/lu";
+import { CiLock } from "react-icons/ci";
+import { IoHelpCircleOutline } from "react-icons/io5";
+import { HiMiniBolt } from "react-icons/hi2";
+import { TbTriangleSquareCircle } from "react-icons/tb";
+
+export default function Sidebar() {
+    const [openCareer , setOpenCareer] = useState(false)
+    const [openResource , setOpenResource] = useState(false)
+    const [openTools , setOpentools] = useState(false)
+  return (
+    <div>
+      <aside
+        id="logo-sidebar"
+        className="fixed top-0 left-0 z-40 w-80 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+          <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                href="#"
+                className="flex font-bold text-[18px] items-center p-2 text-blue-950 rounded-lg dark:text-white hover:text-blue-800 dark:hover:bg-gray-700 group"
+              >
+                <SlHome className="text-xl" />
+                <span className="ms-3">My Dashboard</span>
+              </a>
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={() => setOpenCareer(!openCareer)}
+                className="flex font-bold text-[18px] items-center w-full p-2 text-base text-blue-950 transition duration-75 rounded-lg group hover:text-blue-800 dark:text-white dark:hover:bg-gray-700 cursor-pointer"
+              >
+                <HiOutlineSquares2X2 className="text-xl" />
+                <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
+                  My Learning Journey
+                </span>
+                {openCareer ? (
+                  <FaAngleUp className="text-blue-800" />
+                ) : (
+                  <FaChevronDown className="text-blue-800" />
+                )}
+              </button>
+              {openCareer && (
+                <ul id="dropdown-career" className="py-2 space-y-2">
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    >
+                      In-Progress Courses
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    >
+                      Saved Content & Notes
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    >
+                      Transcripts & Certificates
+                    </a>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex font-bold text-[18px] items-center p-2 text-blue-950 rounded-lg dark:text-white hover:text-blue-800 dark:hover:bg-gray-700 group"
+              >
+                <MdOutlineCardTravel className="text-xl" />
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Careers & Roles
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex font-bold text-[18px] items-center p-2 text-blue-950 rounded-lg dark:text-white hover:text-blue-800 dark:hover:bg-gray-700 group"
+              >
+                <PiCertificateLight className="text-xl" />
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Get Certified
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex font-bold text-[18px] items-center p-2 text-blue-950 rounded-lg dark:text-white hover:text-blue-800 dark:hover:bg-gray-700 group"
+              >
+                <GrCatalogOption className="text-xl" />
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Course Catalog
+                </span>
+              </a>
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={() => setOpenResource(!openResource)}
+                className="flex font-bold text-[18px] items-center w-full p-2 text-base text-blue-950 transition duration-75 rounded-lg group hover:text-blue-800 dark:text-white dark:hover:bg-gray-700 cursor-pointer"
+              >
+                <LuFilePenLine className="text-xl" />
+                <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
+                  Resource Center
+                </span>
+                {openResource ? (
+                  <FaAngleUp className="text-blue-800" />
+                ) : (
+                  <FaChevronDown className="text-blue-800" />
+                )}
+              </button>
+              {openResource && (
+                <ul id="dropdown-resource" className="py-2 space-y-2">
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    >
+                      Articles
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    >
+                      Templates
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    >
+                      Skills Assessments
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 gap-2"
+                    >
+                      Case Study Challenges
+                      <CiLock />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    >
+                      Podcasts
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    >
+                      Webinars
+                    </a>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex font-bold text-[18px] items-center p-2 text-blue-950 rounded-lg dark:text-white hover:text-blue-800 dark:hover:bg-gray-700 group"
+              >
+                <FaUsers className="text-xl" />
+                <span className="flex-1 ms-3 whitespace-nowrap inline-flex items-center gap-2">
+                  Community
+                  <CiLock />
+                </span>
+              </a>
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={() => setOpentools(!openTools)}
+                className="flex items-center w-full p-2 font-bold text-[18px] text-blue-950 transition duration-75 rounded-lg group hover:text-blue-800 dark:text-white dark:hover:bg-gray-700 cursor-pointer"
+              >
+                <TbTriangleSquareCircle className="text-xl" />
+                <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
+                  Full-Immersion Tools
+                </span>
+                {openTools ? (
+                  <FaAngleUp className="text-blue-800" />
+                ) : (
+                  <FaChevronDown className="text-blue-800" />
+                )}
+              </button>
+              {openTools && (
+                <ul id="dropdown-example" className="py-2 space-y-2">
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 gap-2"
+                    >
+                      Ask an Expert
+                      <CiLock />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 gap-2"
+                    >
+                      Partnership Deals <CiLock />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 gap-2"
+                    >
+                      Financial Modal Feedback
+                      <CiLock />
+                    </a>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <p className="bg-amber-600 rounded py-3 px-2 text-white text-center my-12 mx-2 flex justify-center items-center gap-3 cursor-pointer">
+              <HiMiniBolt className="text-2xl" />
+              Get Full Access
+            </p>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-blue-950 rounded-lg dark:text-white hover:text-blue-800 dark:hover:bg-gray-700 group"
+              >
+                <IoHelpCircleOutline className="text-xl" />
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Help Center
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-blue-950 rounded-lg dark:text-white hover:text-blue-800 dark:hover:bg-gray-700 group"
+              >
+                <MdOutlineWbIncandescent className="text-xl" />
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  What's New
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+    </div>
+  );
+}
